ff7ac10d0c464f164c6e13f461548dfb7a09fc2d
Dxw630 <dxw630@miami.edu>
2025-02-18 13:27:12 -0500

    added titles to tabs

---
+++ Miami/lib/handler.js
@@ -0,0 +1,71 @@
+let eList = require('../data/emails.json')
+// Import file system module
+
+const fs = require('fs')
+exports.newsletterSignup = (req,res) => {
+    res.render('newsletter-signup', {csfr:'supersecret'})
+}
+
+exports.newsletterSignupProcess = (req,res) => {
+    console.log (req.body)
+    //Extract the data
+   // let user ={
+   //    firstname: req.body.firstname,
+   //     lastname: req.body.lastname,
+   //     adress: req.body.address,
+   //     city: req.body.city,
+   //     state: req.body.state,
+   //     zip: req.body.zip,
+   //     email: req.body.email
+  //  }
+    //add the user to our list
+    eList.users.push (req.body)
+    console.log (eList)
+    let json = JSON.stringify(eList)
+
+
+    fs.writeFileSync('./date/emails.jsonn',json,'utf-8',()=>{})
+
+    res.redirect(303,'/newsletter/list')
+   // res.render('newsletter-signup', {csfr:'supersecret'})
+}
+
+exports.newsletterSignuplist = (req,res) => {
+    console.log(eList)
+    res.render('userspage', {"users":eList.users})
+}
+
+exports.newsletterSignupList=(req,res)=> {
+    let eList = require('../data/emails.json')
+
+console.log(eList)
+    res.render('userspage',{"users":eList.users,
+     nav:navigation
+    })
+}
+
+exports.newsletterUser = (req,res)=>{
+    console.log(eList)
+    let userDetails = eList.users.filter((user)=>{
+        return user.email === req.params.email
+    })
+  console.log(userDetails)
+  res.render('userdetails',{"users": userDetails, nav:navigation})
+}
+
+exports.newsletterUserDelete = (req,res) => {
+    //Create a temporary list (object)
+    let newsList ={}
+    //retrieve all users filtering out the email we don't want
+    // !=not equal
+     newsList.users = eList.users.filter((user)=>{
+        return user.email != req.params.email
+     })
+     console.log("Deleting"+ req.params.email)
+     // convert the object to a string before writing to the file
+     let json= JSON.stringify(newsList)
+
+     fs.writeFileSync('./data/emails.json',json,'utf-8'),()=>{}
+
+     res.redirect(303,'/newsletter/list')
+}
--- Miami/miami.js
+++ Miami/miami.js
@@ -2,8 +2,12 @@ const express = require('express');
 const handlebars = require ('express-handlebars');
 
 const app = express();
+const bodyParser = require('body-parser')
 const port = process.env.port || 3000;
 const navigation = require('./data/navigation.json');
+app.use(bodyParser.urlencoded({extended:true}))
+
+const handler= require('./lib/handler')
 
 //Set up handlebars
 app.engine('handlebars', handlebars.engine());
@@ -15,31 +19,50 @@ app.set("views", __dirname + '/views'); // Ensure express knows where views are
 
 app.get('/', (request, response) => {
     response.type("text/html");
-    response.render("home", { title: "Miami Travel Site", navigation });
+    response.render("home", { title: "Miami Travel Site", nav: navigation });
 });
 
 
 app.get('/beaches', (request, response) => {
     response.type("text/html");
-    response.render("page", { title: "Miami Beaches" });
+    response.render("page", { title: "Miami Beaches", nav: navigation });
 });
 
 app.get('/nightlife', (request, response) => {
     response.type("text/html");
-    response.render("page", { title: "Miami Night Life" });
+    response.render("page", { title: "Miami Night Life", nav: navigation });
 });
 
 app.get('/about', (request, response) => {
     response.type("text/html");
-    response.render("page", { title: "About Miami" });
+    response.render("page", { title: "About Miami", nav: navigation });
 });
 
 app.get('/search', (request, response) => {
     console.log(request);
     response.type("text/html");
     response.render("page", { title: "Search results for: " + request.query.q });
-});
+ })
 
+
+app.get('/basic',(req,res) =>{
+    res.render('page', {req})
+})
+
+
+
+//Newsletter Routes
+app.get('/newsletter-signup', handler.newsletterSignup)
+app.post('/newsletter-signup/process', handler.newsletterSignupProcess)
+app.get('/newsletter/list',handler.newsletterSignupList)
+//Dyanmic Routes
+//details shows one record
+app.get('/newsletter/details/:email',handler.newsletterUser)
+//delete users by email
+app.get('/newsletter/delete/:email',handler.newsletterUserDelete)
+
+//error handling goes after the actual routes
+//The default response is not found 
 app.use((request, response) => {
     response.type("text/html");
     response.status(404);
